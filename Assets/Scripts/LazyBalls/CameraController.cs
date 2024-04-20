fbee9842fc8c0a83a70f0a0174e58676a63833ad
mig32 <mig32@list.ru>
2024-04-21 01:34:14 +0300

    обновление положения камеры при изменении размера окна

--- Assets/Scripts/LazyBalls/CameraController.cs
+++ Assets/Scripts/LazyBalls/CameraController.cs
@@ -1,6 +1,5 @@
 using System;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace LazyBalls
 {
@@ -10,13 +9,27 @@ namespace LazyBalls
         [SerializeField] private float aspectRatioToFit = 0.5f;
         [SerializeField] private float zoomOutKoeff = 0.5f;
 
+        private Vector3 _startPos;
+        private float _savedAspect;
+
         private void Start()
         {
+            _startPos = thisCamera.transform.position;
+        }
+
+        private void Update()
+        {
+            if (Math.Abs(_savedAspect - thisCamera.aspect) < float.Epsilon)
+            {
+                return;
+            }
+
+            _savedAspect = thisCamera.aspect;
             if (thisCamera.aspect < aspectRatioToFit)
             {
                 var ratio = (aspectRatioToFit / thisCamera.aspect) - 1;
                 var cameraTransform = thisCamera.transform;
-                cameraTransform.position -= zoomOutKoeff * ratio * cameraTransform.forward;
+                cameraTransform.position = _startPos - zoomOutKoeff * ratio * cameraTransform.forward;
             }
         }
     }
