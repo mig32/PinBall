2dde8485d4ec016cc8cd2ca0c9ed169d36aa3a34
mig32 <mig32@list.ru>
2024-04-21 14:45:22 +0300

    поправлено зум в зависимости от разрешения

--- Assets/Scripts/LazyBalls/CameraController.cs
+++ Assets/Scripts/LazyBalls/CameraController.cs
@@ -1,36 +1,110 @@
-using System;
+using System.Collections;
 using UnityEngine;
 
 namespace LazyBalls
 {
     public class CameraController : MonoBehaviour
     {
-        [SerializeField] private Camera thisCamera;
-        [SerializeField] private float aspectRatioToFit = 0.5f;
-        [SerializeField] private float zoomOutKoeff = 0.5f;
-
-        private Vector3 _startPos;
-        private float _savedAspect;
-
+        [SerializeField] private Camera _thisCamera;
+        [SerializeField] private BoxCollider _colliderToTrack;
+        [SerializeField] private float _accuracity = 0.01f;
+        [SerializeField] private float _zoomSpeed = 0.1f;
+        
+        private int _savedScreenWidth;
+        private int _savedScreenHeight;
+        private Coroutine _zoomCoroutine;
+        
         private void Start()
         {
-            _startPos = thisCamera.transform.position;
+            if (_thisCamera == null)
+            {
+                _thisCamera = GetComponent<Camera>();
+            }
         }
 
         private void Update()
         {
-            if (Math.Abs(_savedAspect - thisCamera.aspect) < float.Epsilon)
+            if (_savedScreenWidth == Screen.width && _savedScreenHeight == Screen.height)
             {
                 return;
             }
 
-            _savedAspect = thisCamera.aspect;
-            if (thisCamera.aspect < aspectRatioToFit)
+            _savedScreenWidth = Screen.width;
+            _savedScreenHeight = Screen.height;
+            if (_zoomCoroutine != null)
             {
-                var ratio = (aspectRatioToFit / thisCamera.aspect) - 1;
-                var cameraTransform = thisCamera.transform;
-                cameraTransform.position = _startPos - zoomOutKoeff * ratio * cameraTransform.forward;
+                StopCoroutine(_zoomCoroutine);
             }
+
+            _zoomCoroutine = StartCoroutine(ZoomCoroutine());
+        }
+
+        private IEnumerator ZoomCoroutine()
+        {
+            var zoomDirection = 0.0f;
+            do
+            {
+                var bounds = _colliderToTrack.bounds;
+                var center = bounds.center;
+                var firstPoint = _thisCamera.WorldToScreenPoint(center + bounds.extents);
+                var points = new Vector3[7];
+                points[0] = _thisCamera.WorldToScreenPoint(center - bounds.extents);
+                var shift = bounds.extents;
+                shift.x = -shift.x;
+                points[1] = _thisCamera.WorldToScreenPoint(center + shift);
+                points[2] = _thisCamera.WorldToScreenPoint(center - shift);
+                shift = bounds.extents;
+                shift.y = -shift.y;
+                points[3] = _thisCamera.WorldToScreenPoint(center + shift);
+                points[4] = _thisCamera.WorldToScreenPoint(center - shift);
+                shift = bounds.extents;
+                shift.z = -shift.z;
+                points[5] = _thisCamera.WorldToScreenPoint(center + shift);
+                points[6] = _thisCamera.WorldToScreenPoint(center - shift);
+
+                var minX = firstPoint.x;
+                var maxX = firstPoint.x;
+                var minY = firstPoint.y;
+                var maxY = firstPoint.y;
+                foreach (var p in points)
+                {
+                    if (p.x < minX)
+                    {
+                        minX = p.x;
+                    }
+
+                    if (p.x > maxX)
+                    {
+                        maxX = p.x;
+                    }
+
+                    if (p.y < minY)
+                    {
+                        minY = p.y;
+                    }
+
+                    if (p.y > maxY)
+                    {
+                        maxY = p.y;
+                    }
+                }
+
+                var fx = 1 - (maxX - minX) / Screen.width;
+                var fy = 1 - (maxY - minY) / Screen.height;
+                if (fx < 0 || fy < 0)
+                {
+                    zoomDirection = Mathf.Min(fx, fy);
+                }
+                else
+                {
+                    zoomDirection = Mathf.Max(fx, fy);
+                }
+
+                var cameraTransform = _thisCamera.transform;
+                cameraTransform.position += cameraTransform.forward * zoomDirection * _zoomSpeed;
+                yield return null;
+            } 
+            while (Mathf.Abs(zoomDirection) > _accuracity);
         }
     }
 }
